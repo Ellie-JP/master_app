27c0de52366fd61265bc27649542a54d57861b2c
Ellie <48361151+Ellie-JP@users.noreply.github.com>
2021-08-03 16:03:23 +0900

    Create login.js

---
+++ master_thesis/app/js/login.js
@@ -0,0 +1,13 @@
+function verify(id) {
+  let result;
+  if (id === 1) {
+    result = 'Doctor';
+  } else {
+    result = 'NOT Doctor';
+  }
+  return result;
+}
+
+console.log(result);
+//console.log(testNum(-5));
+// expected output: "NOT positive"
