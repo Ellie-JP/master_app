47dd835581df08e82f211ef113606dc65df5fffd
Ellie <48361151+Ellie-JP@users.noreply.github.com>
2021-08-03 16:18:11 +0900

    Update login.js

--- master_thesis/app/js/login.js
+++ master_thesis/app/js/login.js
@@ -11,3 +11,18 @@ function verify(id) {
 console.log(result);
 //console.log(testNum(-5));
 // expected output: "NOT positive"
+//const express = require('express');
+//const app = express();
+
+// CSSや画像ファイルを置くフォルダを指定するコードを貼り付けてください
+//app.use(express.static('public'));
+
+//app.get('/', (req, res) => {
+//  res.render('hello.ejs');
+//});
+
+//app.get('/top', (req, res) => {
+//  res.render('top.ejs');
+//});
+
+//app.listen(3000);
